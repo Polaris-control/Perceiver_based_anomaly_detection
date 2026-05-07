768918161a4de781713fa87326d7d32f98d8a74f
Polaris <hukaifeng@pusan.ac.kr>
2026-05-07 12:59:21 +0900

    更新异常检测模型包导出接口

---
+++ perceiver-io/perceiver/model/vision/anomaly_detector/__init__.py
@@ -0,0 +1,7 @@
+from perceiver.model.vision.anomaly_detector.backend import (
+    AnomalyDecoderConfig,
+    AnomalyDetector,
+    AnomalyDetectorConfig,
+    AnomalyEncoderConfig,
+)
+from perceiver.model.vision.anomaly_detector.lightning import LitAnomalyDetector
